06011b21f4dc677a14d29b6bb004cd78d6d9dae2
Stephanto <v.stephanto@gmail.com>
2023-10-12 12:27:33 -0300

    Aula 3

---
+++ src/main/java/com/stephanto/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,70 @@
+package com.stephanto.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.stephanto.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+                var servletPath = request.getServletPath();
+
+                if(servletPath.equals("/tasks/")){
+                    // Pegar a autenticação
+                    var authorization = request.getHeader("Authorization");
+
+                    var authEncoded = authorization.substring("Basic".length()).trim();
+
+                    byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+                    var authString = new String(authDecoded);
+
+                    String[] credentials = authString.split(":");
+
+                    var username = credentials[0];
+                    var password = credentials[1];
+
+                    //Validar usuário
+                    var user = this.userRepository.findByUsername(username);
+                    if(user == null){
+                        response.sendError(401);
+                        return;
+                    }else{
+                        //Validar senha
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if(passwordVerify.verified){
+
+                            filterChain.doFilter(request, response);
+                        }else{
+                            response.sendError(500);
+                        }
+                    }
+                }else{
+                    filterChain.doFilter(request, response);
+                }
+
+
+
+    }
+
+
+    
+}
--- src/main/java/com/stephanto/todolist/user/UserModel.java
+++ src/main/java/com/stephanto/todolist/user/UserModel.java
@@ -2,8 +2,30 @@
 
 package com.stephanto.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+// Lombok é uma biblioteca que permite a criação de getters e setters, construtores, equals, hashcode e toString de forma automática.
+// @Data é uma anotação do Lombok que cria os getters e setters, construtores, equals, hashcode e toString de forma automática.
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    private String name;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String username;
+    private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
